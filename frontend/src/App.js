3a93e36e041fd10ba6e102485b9d0df4b00b6ff0
Gokul Devarajan <gokuldevaraj4@gmail.com>
2025-05-27 11:03:52 -0400

    Add React frontend with 'It works' sample

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      height: '100vh',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '2rem'
+    }}>
+      It works
+    </div>
+  );
+}
+
+export default App;
